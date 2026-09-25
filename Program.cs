d8b1680f4ccb0190154faefc1f9556b3e76e8c86
DESKTOP-4A7G64Q\User <user@etec.com>
2026-09-25 20:08:12 -0300

    Finalizar Programa

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("--- Primero Caractere ---\n");
+
+Console.Write("Digite algo...: ");
+string textoDigitado = Console.ReadLine()!;
+
+Console.WriteLine(textoDigitado.Trim().Substring(0, 1));
